1776aa0e4979425ff02948ffd3623e0106d630e7
dana9986655 <danaalb33@gmail.com>
2026-02-10 15:08:31 +0300

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,14 @@
+async function setLanguage(lang){
+  const res = await fetch(`lang/${lang}.json`);
+  const data = await res.json();
+  document.querySelectorAll("[data-lang]").forEach(el=>{
+    const key = el.getAttribute("data-lang");
+    if(data[key]) el.textContent = data[key];
+  });
+  document.body.dir = (lang === "ar") ? "rtl" : "ltr";
+  localStorage.setItem("lang", lang);
+}
+window.onload = ()=>{
+  const lang = localStorage.getItem("lang") || "en";
+  setLanguage(lang);
+};
